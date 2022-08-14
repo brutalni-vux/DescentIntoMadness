6b76e7a696770183d351a881509b58fecdfd90d0
Borna Vukorepa <36900101+brutalni-vux@users.noreply.github.com>
2022-08-14 23:18:21 +0200

    Update X1p(16)(Q_2(i)).m

--- X1p(16)(Q_2(i)).m
+++ X1p(16)(Q_2(i)).m
@@ -1,3 +1,5 @@
+//pronalazimo neke točke na X1^p(16)(Q_2(i))
+
 _<x>:=PolynomialRing(Rationals());
 
 Q2 := pAdicField(2, 60);
@@ -6,6 +8,7 @@ Q2_i<t> := ext<Q2 | x^2 + 2*x + 2>;
 i := t + 1;
 cntFail := 0;
 
+//slutnja da -i + 2 daje točku nad Q2_i kad je p == 3 mod 16
 for p1 in [2..1000] do
     if p1 mod 16 eq 3 then
         x0 := -i + 2;
@@ -18,6 +21,7 @@ for p1 in [2..1000] do
     end if;
 end for;
 
+//slutnja da -3*i + 6 daje točku nad Q2_i kad je p == 11 mod 16
 for p1 in [2..1000] do
     if p1 mod 16 eq 11 then
         x0 := -3*i + 6;
@@ -30,4 +34,5 @@ for p1 in [2..1000] do
     end if;
 end for;
 
+assert cntFail eq 0;
 "Number of fails:", cntFail;
